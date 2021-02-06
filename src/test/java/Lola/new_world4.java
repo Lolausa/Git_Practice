940d0015d550365f8252cfd85bdd799fc221aa85
IlgarA <ilqar.nureddinoglu@gmail.com>
2021-02-06 14:08:20 -0500

    New World 4 is created, where are you?

---
+++ src/test/java/Lola/new_world4.java
@@ -0,0 +1,9 @@
+package Lola;
+
+public class new_world4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("New world --> 4 is coming fast, go run, runnnnnn");
+    }
+}
