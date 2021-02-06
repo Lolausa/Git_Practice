57ff6c2d5af8130f016d258b420752f01c769524
IlgarA <ilqar.nureddinoglu@gmail.com>
2021-02-06 14:09:34 -0500

    New World 4 is created,we are on the same page, no worries at all:)

--- src/test/java/Lola/new_world4.java
+++ src/test/java/Lola/new_world4.java
@@ -5,5 +5,7 @@ public class new_world4 {
     public static void main(String[] args) {
 
         System.out.println("New world --> 4 is coming fast, go run, runnnnnn");
+        //Are we there yet?
+        //Are we there yet, i asked to you?
     }
 }
